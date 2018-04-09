6ca0ced8f3983e1b9b0ad95e25a7b6b7e7c9671b
songzigw <songzigw@163.com>
2018-04-09 18:14:20 +0800

    songmsso

--- songmsso-service/src/main/java/cn/songm/sso/json/JsonUtilsInit.java
+++ songmsso-service/src/main/java/cn/songm/sso/json/JsonUtilsInit.java
@@ -4,13 +4,13 @@ import java.util.Date;
 
 import com.google.gson.GsonBuilder;
 
-import cn.songm.common.utils.DateTypeAdapter;
+import cn.songm.common.utils.GsonDateTypeAdapter;
 import cn.songm.common.utils.JsonUtils;
 
 public class JsonUtilsInit {
 
     public static void initialization() {
-        JsonUtils.init(new GsonBuilder()
-                .registerTypeAdapter(Date.class, new DateTypeAdapter()));
+        JsonUtils.init(new GsonBuilder().registerTypeAdapter(Date.class,
+                new GsonDateTypeAdapter()));
     }
 }
